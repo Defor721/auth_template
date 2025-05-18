3b9993345aab204198fd6adb3e3d318d45ed1715
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-05-18 22:06:04 +0900

    Sentry 세팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  Sentry.init({
+    dsn: '',
+    tracesSampleRate: 1.0, //성능 측정(0~1사이)
+    profileSessionSampleRate: 1.0, // 프로파일링 샘플 비율
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
